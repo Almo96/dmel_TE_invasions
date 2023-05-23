02669ed2c3c274392a9431d27fc4f811e4403d04
Robert Kofler <rokofler@pgnmc008.vu-wien.ac.at>
2023-05-23 14:29:46 +0200

    new analysis -> waves

---
+++ 2023-05-five-dmel/01-repeat-analysis.md
@@ -0,0 +1,84 @@
+Old fragmented copies
+================
+
+# which assemblies
+
+``` bash
+md5 *fasta                                                                                                                                              
+MD5 (Canton-S.fasta) = 4b60ff2ee57267a22585bd86bb614781
+MD5 (DGRP-732.fasta) = b1d5cc5a6a8f8e9be435d66d7b99b2e5
+MD5 (Iso1.fasta) = db53fe4735abe68d6caf8d8ee5d0e10e
+MD5 (Oregon-R.fasta) = 612e225c236129d9f6f15cc8ccd825b6
+MD5 (Pi2.fasta) = 761f82144798cbdb1da0ff40e866d365
+MD5 (SZ244.fasta) = 0426240259ecfa931f6f6e29c48fdd45
+MD5 (SZ45.fasta) = 2731029f2c78a6fd2face83807cc0761
+```
+
+# Repeat Masking
+
+``` bash
+for i in *.fa; do RepeatMasker -pa 20 -no_is -s -nolow -dir out -lib repeatlibrary/teseqs.fasta  $i;done  
+```
+
+Defragment files
+
+``` bash
+for i in *ori.out; do python ../info/rm-defragmenter.py --rm $i --fai ../info/teseqs.fasta.fai --dist 100 > ../raw-defrag/$i.def; done
+```
+
+Append file name and merge
+
+``` bash
+awk '{print $0,FILENAME}' *.ori.out.def|perl -pe 's/\.fasta\.ori\.out\.def//' > merged.def   
+```
+
+# Visualize
+
+``` r
+#  RT1C X_RaGOO +   1243825 1244687 10.6474314214   802 5443 Canton-S
+#  RT1C X_RaGOO +   20257895    20259126    12.0255913978   1302    5443 Canton-S
+#  RT1C X_RaGOO +   20267696    20268986    11.9096513353   1348    5443 Canton-S
+library(tidyverse)
+```
+
+    ## ── Attaching packages ─────────────────────────────────────── tidyverse 1.3.1 ──
+
+    ## ✔ ggplot2 3.3.6     ✔ purrr   0.3.4
+    ## ✔ tibble  3.1.7     ✔ dplyr   1.0.9
+    ## ✔ tidyr   1.2.0     ✔ stringr 1.4.0
+    ## ✔ readr   2.1.2     ✔ forcats 0.5.1
+
+    ## ── Conflicts ────────────────────────────────────────── tidyverse_conflicts() ──
+    ## ✖ dplyr::filter() masks stats::filter()
+    ## ✖ dplyr::lag()    masks stats::lag()
+
+``` r
+theme_set(theme_bw())
+
+h<-read.table("/Users/rokofler/analysis/dmel_TE_invasions/2023-05-five-dmel/raw-defrag/merged.def",header=F)
+names(h)<-c("te","chr","strand","start","end","div","fraglen","telen","sample")
+h$lenfraction <- h$fraglen / h$telen
+# https://ggplot2.tidyverse.org/reference/geom_hex.html
+h<-subset(h,fraglen>200)
+h<-subset(h,sample %in% c("Canton-S","DGRP-732","Iso1","Pi2"))
+h$sample<-factor(h$sample, levels=c("Canton-S", "Iso1", "DGRP-732", "Pi2"))
+tmuseum<-subset(h,te %in% c("OPUS","BLOOD","412"))
+tall<-subset(h,te %in% c("PPI251","DMHFL1","DMIFACA","TIRANT","OPUS","412","BLOOD"))
+tall$te<-factor(tall$te, levels=c("412","BLOOD","OPUS", "TIRANT", "DMIFACA","DMHFL1","PPI251"))
+levels(tall$te) <- c('412', 'Blood', 'Opus',"Tirant","I-element","Hobo","P-element")
+
+p <- ggplot(tall, aes(div, lenfraction)) + geom_hex(bins=10)+facet_grid(sample~te)+
+  scale_fill_gradient2(limits = c(0, 30), oob = scales::squish)+xlab("divergence")+ylab("length fraction of full-length insertion")
+plot(p)
+```
+
+![](01-repeat-analysis_files/figure-gfm/unnamed-chunk-5-1.png)<!-- -->
+
+``` r
+pdf(file="/Users/rokofler/analysis/dmel_TE_invasions/2023-05-five-dmel/graphs/all-separate.pdf",width=8,height=7)
+plot(p)
+dev.off()
+```
+
+    ## quartz_off_screen 
+    ##                 2
---
+++ 2023-05-five-dmel/info/rm-defragmenter.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+import os
+import sys
+import inspect
+import re
+import argparse
+import random
+import math
+import collections
+
+class TEInsertion:
+	def __init__(self,fam,chrm,start,end,strand,div,qstart,qend):
+		self.fam=fam
+		self.chrm=chrm
+		self.start=start
+		self.strand=strand
+		self.end=end
+		self.div=div
+		self.qstart=qstart
+		self.qend=qend
+
+class TEBuilder:
+	def __init__(self,length,fam,strand,chrm,start):
+		self._seq=[0]*(length+1)
+		self._con=[]
+		self.maxend=0
+		self._len=length
+		self._fam=fam
+		self._strand=strand
+		self._chrm=chrm
+		self._start=start
+
+	def add(self,te):
+		assert(te.fam==self._fam)
+		assert(te.strand==self._strand)
+		assert(te.chrm==self._chrm)
+		start,end,div=te.qstart,te.qend,te.div
+		self._con.append((start,end,div))
+		for i in range(start,end+1):
+			self._seq[i]=1
+
+		if te.end > self.maxend:
+			self.maxend=te.end
+	
+	def length(self):
+		return sum(self._seq)
+
+	def avdiv(self):
+		score=0.0
+		tol=0
+		for s,e,d in self._con:
+			ems=e-s+1
+			tol+=ems
+			score+=ems*d
+		av=score/float(tol)
+		return av
+	
+	def __str__(self):
+		topr=[self._fam, self._chrm, self._strand, str(self._start), str(self.maxend),str(self.avdiv()),str(self.length()),str(self._len)]
+		return "\t".join(topr)
+
+
+
+def read_rm(rmfile):
+	reli=collections.defaultdict(lambda:collections.defaultdict(lambda:collections.defaultdict(lambda:[])))
+	for l in open(rmfile):
+		l=l.strip()
+		a=re.split("\s+",l)
+		# 0			1		2		3		4					5	6		7		8		9			10			11			12		13
+		# ['997', '4.88', '0.00', '0.00', '211000022278031', '345', '467', '(554)', 'C', 'DM06920', 'Unspecified', '(4358)', '1725', '1603']
+		score,div,chrm,start,end,strand,fam = int(a[0]),float(a[1]),a[4],int(a[5]),int(a[6]),a[8],a[9]
+		raw1,raw2,raw3=a[11],a[12],a[13]
+		qstart,qend=0,0
+		if(strand=="C"):
+			qstart,qend=int(raw3),int(raw2)
+		elif(strand=="+"):
+			qstart,qend=int(raw1),int(raw2)
+		else:
+			raise Exception("Invalid")
+		#  26532 0.00 0.31 0.31             2L_RaGOO   3975229   3978135 (22168013) +               PPI251     Unspecified       1    2907     (0)    
+ 		#   1117 0.82 0.00 0.00             2L_RaGOO   4095206   4095327 (22050821) +               PPI251     Unspecified       1     122  (2785)  
+		#  26711 0.03 0.24 0.17             2L_RaGOO  25681458  25684362 (461786) C               PPI251     Unspecified     (0)    2907       1    
+		#   4617 0.38 0.57 0.00             2L_RaGOO  24790272  24790799 (1355349) 	C               PPI251     Unspecified     (0)    2907    2377    
+  		#	3610 0.00 0.51 0.00             2L_RaGOO  24790789  24791182 (1354966) 	C               PPI251     Unspecified  (2493)     414      19    
+		assert(end>start)
+		assert(qend>qstart)
+		te=TEInsertion(fam,chrm,start,end,strand,div,qstart,qend)
+		reli[fam][chrm][strand].append(te)
+	return reli
+
+def readfai(fai):
+	"""
+		M14653	1286	8	80	81
+		DME9736	7411	1320	80	81
+	"""
+	lh={}
+	for l in open(fai):
+		a=l.split("\t")
+		te,telen=a[0],int(a[1])
+		lh[te]=telen
+	return lh
+ 
+
+parser = argparse.ArgumentParser(description="""           
+Description
+-----------
+Summary statistics
+""",formatter_class=argparse.RawDescriptionHelpFormatter,
+epilog="""
+
+Authors
+-------
+    Robert Kofler
+""")
+parser.add_argument('--fai', type=str, default=None, dest="fai", required=True, help="A fai file")
+parser.add_argument("--rm", type=str, required=True, dest="rm", default=None, help="repeat masker file")
+parser.add_argument("--dist", type=int, required=True, dest="dist", default=None, help="distance")
+args = parser.parse_args()
+
+teh=readfai(args.fai)
+rm=read_rm(args.rm)
+dist=args.dist
+
+
+for fam,temp1 in rm.items():
+	for chrm,temp2 in temp1.items():
+		for strand,insertions in temp2.items():
+			insertions=sorted(insertions,key=lambda i: i.start)
+			first=insertions.pop(0)
+			telen=teh[first.fam]
+			tebuilder=TEBuilder(telen,first.fam,strand,chrm,first.start)
+			tebuilder.add(first)
+			for t in insertions:
+				if tebuilder.maxend+dist >= t.start:
+					tebuilder.add(t)
+				else:
+					print(str(tebuilder))
+					tebuilder=TEBuilder(telen,t.fam,strand,chrm,t.start)
+					tebuilder.add(t)
+			print(str(tebuilder))
+
+
+
